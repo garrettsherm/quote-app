bda295efc1cd267f2f3adce6842d942102dce958
Garrett S <garrettsherm@gmail.com>
2018-06-30 02:24:49 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="quote-app">
+        <header id="title-header" className="mt-3">
+          <div className="container">
+            <div className="row">
+              <h1 className="mx-auto">Window Quote App</h1>
+            </div>
+          </div>
         </header>
-        <p className="App-intro">
-          
-        </p>
+        <div id="app-container" className="container">
+          <div className="row">
+            <div className="col-8">
+                <div className="container-fluid">
+                  <div className="row-fluid">
+                    <div className="col-12">
+                      <h2 id="form-title">Let's begin</h2>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div className="col-4">
+              <h2 id="estimate-title">Estimated Cost Is</h2>
+            </div>         
+          </div>
+        </div>
       </div>
     );
   }
